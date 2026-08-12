f950fa26a4a8de9b8c7d399efba0ca19ea445834
Agustin Leitao <agustinleitao@gmail.com>
2026-08-11 21:13:50 -0300

    chore: rename layouts

--- React Native/chat-app/src/app/(drawer)/_layout.tsx
+++ React Native/chat-app/src/app/(drawer)/_layout.tsx
@@ -1,11 +1,11 @@
 import { Drawer } from 'expo-router/drawer';
 
-export default function RootLayout() {
+export default function DrawerLayout() {
   return (
     <>
-      <Drawer screenOptions={{swipeEdgeWidth: 70, headerShown: false}}>
-        <Drawer.Screen name="(home)" options={{ title: 'Home'}} />
-        <Drawer.Screen name="about" options={{ title: 'About'}} />
+      <Drawer screenOptions={{ swipeEdgeWidth: 70, headerShown: false }}>
+        <Drawer.Screen name="(home)" options={{ title: 'Home' }} />
+        <Drawer.Screen name="about" options={{ title: 'About' }} />
       </Drawer>
     </>
   );
